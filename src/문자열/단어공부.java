e888d26cc99fb8c29682ea50f4ca4de6703e142e
haden <80521474+thdefn@users.noreply.github.com>
2023-06-12 12:37:17 +0900

    230612 [문자열]

---
+++ src/문자열/단어공부.java
@@ -0,0 +1,40 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class 단어공부 {
+    public static int[] getAlphabetCount(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                count[ch - 'A']++;
+            } else {
+                count[ch - 'a']++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int[] count = getAlphabetCount(str);
+
+        int maxCount = -1;
+        char maxAlphabet = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if(count[i] > maxCount){
+                maxCount = count[i];
+                maxAlphabet = (char) ('A'+i);
+            }
+            else if(count[i] == maxCount){
+                maxAlphabet = '?';
+            }
+        }
+
+        System.out.println(maxAlphabet);
+    }
+}
