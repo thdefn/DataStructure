755d8d57e8edfbf13eb485d5503854955970454e
haden <80521474+thdefn@users.noreply.github.com>
2023-06-28 09:49:51 +0900

    230628 [완전 탐색]

---
+++ src/완전탐색/유레카이론.java
@@ -0,0 +1,44 @@
+package 완전탐색;
+
+import java.util.Scanner;
+
+public class 유레카이론 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] nums = new int[T];
+        for (int i = 0; i < T; i++) {
+            nums[i] = sc.nextInt();
+        }
+        
+        int[] triNums = new int[1001];
+        int num = 0;
+        for (int i = 1; num < 1000; i++) {
+            num = ((i * (i+1))/2);
+            triNums[i] = num;
+        }
+
+        boolean[] ans = new boolean[T];
+        for (int i = 0; i < T; i++) {
+            for (int j = 1; triNums[j] < nums[i]; j++) {
+                boolean end = false;
+                for (int k = j; triNums[k] < nums[i]; k++) {
+                    for (int l = k; triNums[l] < nums[i] ; l++) {
+                        if(triNums[l] == 0) {
+                            end = true;
+                            break;
+                        }
+                        if (triNums[j] + triNums[k] + triNums[l] == nums[i]){
+                            ans[i] = true;
+                            break;
+                        }
+                    }
+                    if(end) break;
+                }
+                if(end) break;
+            }
+            System.out.println(ans[i] ? 1 : 0);
+        }
+
+    }
+}
