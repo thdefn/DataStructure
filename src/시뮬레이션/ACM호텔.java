9fcd02df410e17dabc7118eb1b31cb0acfa42e63
haden <80521474+thdefn@users.noreply.github.com>
2023-07-08 21:51:01 +0900

    230708 [시뮬레이션]

--- src/시뮬레이션/ACM호텔.java
+++ src/시뮬레이션/ACM호텔.java
@@ -15,8 +15,9 @@ public class ACM호텔 {
             int N = sc.nextInt();
 
             // O(1)
+            // 순서에 대한 연산은 N - 1을 해도 순서가 유지된다
             int distance = (N - 1) / H + 1; // 1호부터 시작하도록 고정 (1을 더해주기)
-            int floor = (N - 1) % H + 1; // 순서를 구하는 것이므로 N - 1 할 때에도 순서가 유지됨
+            int floor = (N - 1) % H + 1;
             System.out.printf("%d%02d\n", floor, distance);
         }
     }
