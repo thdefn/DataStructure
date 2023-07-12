01f8a5aee25fb52b50d73330b1e8c7516279805a
haden <80521474+thdefn@users.noreply.github.com>
2023-07-12 13:01:42 +0900

    230712 [시뮬레이션]

--- src/시뮬레이션/판화.java
+++ src/시뮬레이션/판화.java
@@ -1,50 +1,56 @@
 package 시뮬레이션;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 
+/**
+ * 런타임 에러 (NoSuchElement)
+ * 문자열의 최소 길이가 없음 문자열이 만약 입력되지 않는다면?
+ *
+ * 5
+ *
+ * sc.next(); -> 공백을 제외하고 다음에 오는 문자열을 받음
+ * 아무것도 입력되지 않은 경우 공백을 제외하면 남은 게 없어서 NoSuchElement 익셉션 발생
+ */
 public class 판화 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String trace = br.readLine();
-        char[][] graph = new char[N][N];
+        int N = sc.nextInt();
+        String command = sc.hasNext() ? sc.next() : "";
+        int curR = 0;
+        int curC = 0;
+        boolean[][] passVertical = new boolean[N][N];
+        boolean[][] passHorizontal = new boolean[N][N];
+        for (int i = 0; i < command.length(); i++) {
+            char cmd = command.charAt(i);
+            if (cmd == 'D') {
+                if (curR == N - 1) continue;
+                passVertical[curR][curC] = passVertical[curR + 1][curC] = true;
+                curR++;
+            } else if (cmd == 'U') {
+                if (curR == 0) continue;
+                passVertical[curR][curC] = passVertical[curR - 1][curC] = true;
+                curR--;
+            } else if (cmd == 'L') {
+                if (curC == 0) continue;
+                passHorizontal[curR][curC] = passHorizontal[curR][curC - 1] = true;
+                curC--;
+            } else {
+                if (curC == N - 1) continue;
+                passHorizontal[curR][curC] = passHorizontal[curR][curC + 1] = true;
+                curC++;
+            }
+        }
+
         for (int i = 0; i < N; i++) {
-            Arrays.fill(graph[i], '.');
-        }
-
-        // 좌표
-        int currentX = 0;
-        int currentY = 0;
-        for (int i = 0; i < trace.length(); i++) {
-            char d = trace.charAt(i);
-            if (d == 'U' && (currentY - 1) >= 0) {
-                graph[currentY][currentX] = (graph[currentY][currentX] == '-' | graph[currentY][currentX] == '+') ? '+' : '|';
-                currentY--;
-                graph[currentY][currentX] = (graph[currentY][currentX] == '-' | graph[currentY][currentX] == '+') ? '+' : '|';
-            } else if (d == 'D' && (currentY + 1) < N) {
-                graph[currentY][currentX] = (graph[currentY][currentX] == '-' | graph[currentY][currentX] == '+') ? '+' : '|';
-                currentY++;
-                graph[currentY][currentX] = (graph[currentY][currentX] == '-' | graph[currentY][currentX] == '+') ? '+' : '|';
-            } else if (d == 'L' && (currentX - 1) >= 0) {
-                graph[currentY][currentX] = (graph[currentY][currentX] == '|' | graph[currentY][currentX] == '+') ? '+' : '-';
-                currentX--;
-                graph[currentY][currentX] = (graph[currentY][currentX] == '|' | graph[currentY][currentX] == '+') ? '+' : '-';
-            } else if (d == 'R' && (currentX + 1) < N) {
-                graph[currentY][currentX] = (graph[currentY][currentX] == '|' | graph[currentY][currentX] == '+') ? '+' : '-';
-                currentX++;
-                graph[currentY][currentX] = (graph[currentY][currentX] == '|' | graph[currentY][currentX] == '+') ? '+' : '-';
+            String ans = "";
+            for (int j = 0; j < N; j++) {
+                if (passVertical[i][j] && passHorizontal[i][j]) ans += "+";
+                else if (passVertical[i][j]) ans += "|";
+                else if (passHorizontal[i][j]) ans += "-";
+                else ans += ".";
             }
-        }
-
-        for (char[] arr : graph) {
-            for (char c : arr) {
-                System.out.print(c);
-            }
-            System.out.println();
+            System.out.println(ans);
         }
     }
 }
