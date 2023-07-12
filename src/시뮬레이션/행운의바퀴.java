a219800d49f34ed2fcd9f14eaaa4103476d255b7
haden <80521474+thdefn@users.noreply.github.com>
2023-07-12 10:44:46 +0900

    230712 [시뮬레이션]

--- src/시뮬레이션/행운의바퀴.java
+++ src/시뮬레이션/행운의바퀴.java
@@ -1,19 +1,18 @@
 package 시뮬레이션;
 
-import java.util.Objects;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * A
- * A?B
- * A?B??C
- * <p>
- * C??B?A
- * <p>
- * A
- * A?B
- * A?B????B
- * A?BC?A?B
+ * HONITAVR
+ * H???????
+ * HO??????
+ * HON?????
+ * HON????R
+ * HON??A?R
+ * HON?TA?R
+ * HONITA?R
+ * HONITAVR
  */
 public class 행운의바퀴 {
     public static void main(String[] args) {
@@ -21,41 +20,57 @@ public class 행운의바퀴 {
         int N = sc.nextInt();
         int K = sc.nextInt();
 
-        String before = "";
-        String answer = "";
-        boolean isExist = true;
+        int[] times = new int[K];
+        char[] alpha = new char[K];
         for (int i = 0; i < K; i++) {
-            int times = sc.nextInt();
-            String alpha = sc.next();
-            System.out.println(alpha);
-            if (Objects.equals(alpha, before)) {
-                if (times % N != 0) {
-                    isExist = false;
+            times[i] = sc.nextInt();
+            alpha[i] = sc.next().charAt(0);
+        }
+
+        char[] wheel = new char[N];
+        Arrays.fill(wheel, '?');
+        wheel[0] = alpha[K - 1];
+        int beforeIndex = 0;
+        boolean isExist = true;
+        for (int i = K - 2; i >= 0; i--) {
+            int currentIdx = (beforeIndex + times[i + 1] % N) % N;
+
+            // 바로 전 알파벳이 현재 알파벳이면 인덱스가 같아야 한다
+            if (wheel[beforeIndex] == alpha[i] && currentIdx != beforeIndex) {
+                isExist = false;
+                break;
+            }
+            // 넣으려는 인덱스가 '?'가 아니라면 현재 알파벳과 넣으려는 인덱스의 알파벳이 같아야 한다
+            if (wheel[currentIdx] != '?' && wheel[currentIdx] != alpha[i]) {
+                isExist = false;
+                break;
+            }
+
+            // 현재 알파벳이 다른 인덱스에 있으면 안된다
+            boolean isDuplicated = false;
+            for (int j = 0; j < N; j++) {
+                if (wheel[j] == alpha[i] && j != currentIdx) {
+                    isDuplicated = true;
                     break;
-                }
-            } else {
-                if (times % N == 0) {
-                    isExist = false;
-                    break;
-                } else {
-                    for (int j = 0; j < times - 1; j++) {
-                        answer = answer + "?";
-                    }
-                    answer = answer + alpha;
                 }
             }
-            before = alpha;
+
+            if (isDuplicated) {
+                isExist = false;
+                break;
+            }
+
+            wheel[currentIdx] = alpha[i];
+            beforeIndex = currentIdx;
         }
 
         if (isExist) {
-            char start = answer.charAt(answer.length()-1);
-            System.out.print(start);
-            for (int i = answer.length() - 2; i >= 0; i--) {
-                if(answer.charAt(i) == start) break;
-                System.out.print(answer.charAt(i));
+            for (int i = 0; i < N; i++) {
+                System.out.print(wheel[i]);
             }
         } else {
-            System.out.println("!");
+            System.out.print("!");
         }
     }
+
 }
