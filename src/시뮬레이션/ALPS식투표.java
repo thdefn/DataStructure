83ea35e63cc372a6a290c17fe93ddc98e29a2266
haden <80521474+thdefn@users.noreply.github.com>
2023-07-17 11:16:46 +0900

    230717 [시뮬레이션]

--- src/시뮬레이션/ALPS식투표.java
+++ src/시뮬레이션/ALPS식투표.java
@@ -1,71 +1,80 @@
 package 시뮬레이션;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ALPS식투표 {
+    static class Score {
+        Score(int staffIndex, double scr) {
+            this.staffIndex = staffIndex;
+            this.scr = scr;
+        }
+
+        int staffIndex;
+        double scr;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt();
         int N = sc.nextInt();
-        char[] staffs = new char[N];
-        int[][] scores = new int[N][14];
 
-        int maxScore = 0;
-        int maxIndex = 0;
-        int cnt = 0;
+        // 1. 전체 득표수 5% 미만의 스태프를 후보에서 제외한다.
+        double validCut = X * 0.05;
+        boolean[] validCandidate = new boolean[26];
+        int[] staffVote = new int[26];
+        int candidateCount = 0;
         for (int i = 0; i < N; i++) {
-            char c = sc.next().charAt(0);
-            int numberOfVotes = sc.nextInt();
-            if (numberOfVotes * 20 < X) continue;
-            staffs[cnt] = c;
-            if (numberOfVotes > maxScore) {
-                maxScore = numberOfVotes;
-                maxIndex = cnt;
+            String name = sc.next();
+            int vote = sc.nextInt();
+            if (vote >= validCut) {
+                int index = name.charAt(0) - 'A';
+                validCandidate[index] = true;
+                staffVote[index] = vote;
+                candidateCount++;
             }
-            for (int j = 0; j < 14; j++) {
-                scores[cnt][j] = numberOfVotes / (j + 1);
-            }
-            cnt++;
         }
-
-        char[] alpsStaff = new char[14];
-        Arrays.fill(alpsStaff, staffs[maxIndex]);
-        int[] alpsScore = scores[maxIndex].clone();
-
-        int[] answer = new int[26];
-        Arrays.fill(answer, -1);
-
-        for (int i = 0; i < cnt; i++) {
-            answer[staffs[i] - 'A'] = 0;
-            if (i == maxIndex) continue;
-            // 스태프의 점수를 넣을 위치를 찾는다
-            for (int j = 0; j < 14; j++) {
-                // 알프스 스코어 최소값보다는 스태프의 점수가 커야한다
-                if (alpsScore[13] > scores[i][j]) break;
-                for (int k = 12; k >= 0; k--) {
-                    // 스태프의 점수가 알프스 스코어 점수보다 크면 해당 지점에 삽입한다
-                    if (alpsScore[k] > scores[i][j]) {
-                        for (int l = 13; l >= k + 2; l--) {
-                            alpsScore[l] = alpsScore[l - 1];
-                            alpsStaff[l] = alpsStaff[l - 1];
-                        }
-                        alpsScore[k + 1] = scores[i][j];
-                        alpsStaff[k + 1] = staffs[i];
-                        break;
-                    }
+        // 2. 남은 스태프마다 받은 득표수를 1~14로 나눈 점수 집합을 구한다.
+        // 각각의 점수를 누가 받았는지 알아야 칩을 지급할 수 있다 -> Score 클래스 활용
+        Score[] scores = new Score[candidateCount * 14];
+        int scoreIndex = 0;
+        for (int i = 0; i < 26; i++) {
+            if (validCandidate[i]) {
+                for (int j = 1; j <= 14; j++) {
+                    scores[scoreIndex++] = new Score(i, (double) staffVote[i] / j);
                 }
             }
         }
 
+        // 3. 전체 점수 집합에서 점수가 큰 1~14번째 스태프에게 칩을 한 개씩 지급한다.
+        sortScoresDescendingOrder(scores);
+        int[] ans = new int[26];
         for (int i = 0; i < 14; i++) {
-            answer[alpsStaff[i] - 'A']++;
+            ans[scores[i].staffIndex]++;
         }
 
+        // 4. 스태프 이름에 대해 사전순으로 후보 스태프와 받은 칩의 수를 출력한다.
         for (int i = 0; i < 26; i++) {
-            if (answer[i] == -1) continue;
-            System.out.println((char) (i + 'A') + " " + answer[i]);
+            if (validCandidate[i])
+                System.out.println((char) (i + 'A') + " " + ans[i]);
         }
+    }
 
+    // O(N^2) = O((validateCount * 14)^2)
+    private static void sortScoresDescendingOrder(Score[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // 나보다 작은 애를 찾아서 그 앞에 선다.
+            for (int j = 0; j < i; j++) {
+                // 나보다 작은 애가 내 앞에 있다면
+                if (arr[i].scr > arr[j].scr) {
+                    // j부터 i 이전 값들이 한 칸씩 뒤로 가고, j에 현재값 넣는다
+                    Score cur = arr[i];
+                    for (int k = i; k > j; k--) {
+                        arr[k] = arr[k - 1];
+                    }
+                    arr[j] = cur;
+                    break;
+                }
+            }
+        }
     }
 }
---
+++ src/시뮬레이션/풀이/ALPS식투표.java
@@ -0,0 +1,71 @@
+package 시뮬레이션.풀이;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ALPS식투표 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+        char[] staffs = new char[N];
+        int[][] scores = new int[N][14];
+
+        int maxScore = 0;
+        int maxIndex = 0;
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            char c = sc.next().charAt(0);
+            int numberOfVotes = sc.nextInt();
+            if (numberOfVotes * 20 < X) continue;
+            staffs[cnt] = c;
+            if (numberOfVotes > maxScore) {
+                maxScore = numberOfVotes;
+                maxIndex = cnt;
+            }
+            for (int j = 0; j < 14; j++) {
+                scores[cnt][j] = numberOfVotes / (j + 1);
+            }
+            cnt++;
+        }
+
+        char[] alpsStaff = new char[14];
+        Arrays.fill(alpsStaff, staffs[maxIndex]);
+        int[] alpsScore = scores[maxIndex].clone();
+
+        int[] answer = new int[26];
+        Arrays.fill(answer, -1);
+
+        for (int i = 0; i < cnt; i++) {
+            answer[staffs[i] - 'A'] = 0;
+            if (i == maxIndex) continue;
+            // 스태프의 점수를 넣을 위치를 찾는다
+            for (int j = 0; j < 14; j++) {
+                // 알프스 스코어 최소값보다는 스태프의 점수가 커야한다
+                if (alpsScore[13] > scores[i][j]) break;
+                for (int k = 12; k >= 0; k--) {
+                    // 스태프의 점수가 알프스 스코어 점수보다 크면 해당 지점에 삽입한다
+                    if (alpsScore[k] > scores[i][j]) {
+                        for (int l = 13; l >= k + 2; l--) {
+                            alpsScore[l] = alpsScore[l - 1];
+                            alpsStaff[l] = alpsStaff[l - 1];
+                        }
+                        alpsScore[k + 1] = scores[i][j];
+                        alpsStaff[k + 1] = staffs[i];
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 14; i++) {
+            answer[alpsStaff[i] - 'A']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (answer[i] == -1) continue;
+            System.out.println((char) (i + 'A') + " " + answer[i]);
+        }
+
+    }
+}
