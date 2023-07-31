70efc65857e436ec10ef63c6730a64ad83846443
haden <80521474+thdefn@users.noreply.github.com>
2023-07-31 18:45:41 +0900

    230731 [정렬]

--- src/정렬/좌표압축.java
+++ src/정렬/좌표압축.java
@@ -1,53 +1,77 @@
 package 정렬;
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
+/**
+ * 2차원 배열에 대한 정렬은 Comparator 사용
+ * Map - 특정값에 대한 동일한 계산값 저장
+ * Set - TreeSet 중복 제거
+ */
 public class 좌표압축 {
-    static class Coordinate implements Comparable<Coordinate> {
-        int index;
-        int x;
+    public static void main2(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-        Coordinate(int index, int x) {
-            this.index = index;
-            this.x = x;
+        int N = sc.nextInt();
+        // 1. 입력된 좌표를 작은 순으로 정렬한다.
+        int[] xs = new int[N];
+        Set<Integer> set = new TreeSet<>();
+        // NLog(N)
+        for (int i = 0; i < N; i++) {
+            xs[i] = sc.nextInt();
+            set.add(xs[i]);
         }
 
-        @Override
-        public int compareTo(Coordinate o) {
-            return x - o.x;
-        }
+        // 2. 정렬된 좌표를 중복을 제거하며 압축된 인덱스를 기록한다.
+        // N
+        Map<Integer, Integer> sortedIndex = new HashMap<>();
+        int idx = 0;
+        for (int x : set)
+            sortedIndex.put(x, idx++);
+
+        // 3. 입력된 좌표에 알맞은 압축 인덱스를 출력한다.
+        // N
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++)
+            bw.write(sortedIndex.get(xs[i]) + " ");
+        bw.write("\n");
+        bw.flush();
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Coordinate[] coordinates = new Coordinate[N];
-        String[] nums = br.readLine().split(" ");
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        // 1. 입력된 좌표를 작은 순으로 정렬한다.
+        int[][] xs = new int[N][2]; // 0: x, 1: inputIndex
         for (int i = 0; i < N; i++) {
-            coordinates[i] = new Coordinate(i, Integer.parseInt(nums[i]));
+            xs[i][0] = sc.nextInt();
+            xs[i][1] = i;
         }
-        Arrays.sort(coordinates);
 
-        int[] index = new int[N];
-        int beforeValue = coordinates[0].x;
-        coordinates[0].x = 0;
-        index[coordinates[0].index] = coordinates[0].x;
-        for (int i = 1; i < N; i++) {
-            if (coordinates[i].x == beforeValue) {
-                beforeValue = coordinates[i].x;
-                coordinates[i].x = coordinates[i - 1].x;
-            } else {
-                beforeValue = coordinates[i].x;
-                coordinates[i].x = coordinates[i - 1].x + 1;
+        Arrays.sort(xs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
             }
-            index[coordinates[i].index] = coordinates[i].x;
+        });
+
+        // 2. 정렬된 좌표를 중복을 제거하며 압축된 인덱스를 기록한다.
+        int[] ans = new int[N];
+        int idx = 0;
+        ans[xs[0][1]] = idx;
+        for (int i = 1; i < N; i++) {
+            if (xs[i][0] != xs[i - 1][0]) idx++;
+            ans[xs[i][1]] = idx;
         }
 
+        // 3. 입력된 좌표에 알맞은 압축 인덱스를 출력한다
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < N; i++)
-            bw.write(index[i] + " ");
+            bw.write(ans[i] + " ");
+        bw.write("\n");
         bw.flush();
-
     }
 }
---
+++ src/정렬/풀이/좌표압축.java
@@ -0,0 +1,52 @@
+package 정렬.풀이;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class 좌표압축 {
+    static class Coordinate implements Comparable<Coordinate> {
+        int index;
+        int x;
+
+        Coordinate(int index, int x) {
+            this.index = index;
+            this.x = x;
+        }
+
+        @Override
+        public int compareTo(Coordinate o) {
+            return x - o.x;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Coordinate[] coordinates = new Coordinate[N];
+        String[] nums = br.readLine().split(" ");
+        for (int i = 0; i < N; i++)
+            coordinates[i] = new Coordinate(i, Integer.parseInt(nums[i]));
+        Arrays.sort(coordinates);
+
+        int[] index = new int[N];
+        int beforeValue = coordinates[0].x;
+        coordinates[0].x = 0;
+        index[coordinates[0].index] = coordinates[0].x;
+        for (int i = 1; i < N; i++) {
+            if (coordinates[i].x == beforeValue) {
+                beforeValue = coordinates[i].x;
+                coordinates[i].x = coordinates[i - 1].x;
+            } else {
+                beforeValue = coordinates[i].x;
+                coordinates[i].x = coordinates[i - 1].x + 1;
+            }
+            index[coordinates[i].index] = coordinates[i].x;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++)
+            bw.write(index[i] + " ");
+        bw.flush();
+
+    }
+}
