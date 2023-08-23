1314eef2a14cadb02149d50385bab0036416bdc3
haden <80521474+thdefn@users.noreply.github.com>
2023-08-23 09:37:07 +0900

    230823 [Array/String]

--- src/leet150/RotateArray.java
+++ src/leet150/RotateArray.java
@@ -4,21 +4,21 @@ package leet150;
  * 0 1 2 3 4 5 6
  * -------------
  * 3 4 5 6 0 1 2
- *
+ * 3 4 5 6 7 8 9
  * 0 1
  * ----
  * 1 0
  */
 public class RotateArray {
     public static void main(String[] args) {
-        int[] nums = {1,2};
+        int[] nums = {1, 2};
         rotate(nums, 3);
     }
+
     public static void rotate(int[] nums, int k) {
         int[] origin = nums.clone();
         for (int i = 0; i < nums.length; i++) {
-            int idx = (i + nums.length + k) % nums.length;
-            nums[idx] = origin[i];
+            nums[(i + k) % nums.length] = origin[i];
         }
     }
 }
