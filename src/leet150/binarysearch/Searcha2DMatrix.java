7d75c4084d675b17fff0489e8757726990ed40ab
haden <80521474+thdefn@users.noreply.github.com>
2023-08-28 18:11:21 +0900

    230828 [binary search]

---
+++ src/leet150/binarysearch/Searcha2DMatrix.java
@@ -0,0 +1,29 @@
+package leet150.binarysearch;
+
+public class Searcha2DMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length; // 1
+        int c = matrix[0].length; // 1
+        int left = 0;
+        int right = r * c - 1;
+
+        if (matrix[right / c][right % c] < target || matrix[0][0] > target)
+            return false;
+        else if (matrix[right / c][right % c] == target || matrix[0][0] == target)
+            return true;
+
+        while (left < right) {
+            int idx = (left + right) / 2;
+            if (matrix[idx / c][idx % c] < target)
+                left = idx + 1;
+            else if (matrix[idx / c][idx % c] > target) right = idx;
+            else return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        System.out.println(searchMatrix(matrix, 13));
+    }
+}
